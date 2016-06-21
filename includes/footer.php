6764ea031a1c6caeb06e6807679927cd8ca6514f
Hery RAKOTONARIVO <hery@odity.fr>
2016-06-21 10:05:43 +0300

    Commit changes features/develop

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<!-- Footer
+================================================== -->
+<footer class="footer">
+    <div class="container">
+        <p>Créer et développer par <a href="">Hery Rakotonarivo</a>. Toute reproduction ou reprise de l'idée/contenu de ce site est intérdite.
+        <p>Copyright &copy; <?php echo(date('Y')) ?> <a href="http://opensource.org/licenses/mit-license.html">MIT License</a>.</p>
+    </div>
+</footer>
+
+
+
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+<script src="docs/assets/js/jquery.js"></script>
+<script src="docs/assets/js/bootstrap-transition.js"></script>
+<script src="docs/assets/js/bootstrap-alert.js"></script>
+<script src="docs/assets/js/bootstrap-modal.js"></script>
+<script src="docs/assets/js/bootstrap-dropdown.js"></script>
+<script src="docs/assets/js/bootstrap-scrollspy.js"></script>
+<script src="docs/assets/js/bootstrap-tab.js"></script>
+<script src="docs/assets/js/bootstrap-tooltip.js"></script>
+<script src="docs/assets/js/bootstrap-popover.js"></script>
+<script src="docs/assets/js/bootstrap-button.js"></script>
+<script src="docs/assets/js/bootstrap-collapse.js"></script>
+<script src="docs/assets/js/bootstrap-carousel.js"></script>
+<script src="docs/assets/js/bootstrap-typeahead.js"></script>
+<script src="docs/assets/js/bootstrap-affix.js"></script>
+
+<script src="docs/assets/js/holder/holder.js"></script>
+<script src="docs/assets/js/google-code-prettify/prettify.js"></script>
+
+<script src="docs/assets/js/application.js"></script>
+
+
+<!-- Analytics
+================================================== -->
+<script>
+    var _gauges = _gauges || [];
+    (function() {
+        var t   = document.createElement('script');
+        t.type  = 'text/javascript';
+        t.async = true;
+        t.id    = 'gauges-tracker';
+        t.setAttribute('data-site-id', '4f0dc9fef5a1f55508000013');
+        t.src = '//secure.gaug.es/track.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(t, s);
+    })();
+</script>
+
+</body>
+</html>
