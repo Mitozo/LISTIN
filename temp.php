62b9e28a8f6316b5434c6c7e43cb04268388ac34
Hery RAKOTONARIVO <hery@odity.fr>
2016-06-21 09:15:40 +0300

    first commit

---
+++ temp.php
@@ -0,0 +1,140 @@
+<?php include('includes/header.php'); ?>
+
+    <!-- Subhead
+    ================================================== -->
+
+
+    <div class="container">
+
+        <div class="row marketing">
+            <div class="span4">
+
+                <div class="card hovercard pull-left">
+                    <img src="docs/assets/img/author-cover.jpg" alt=""/>
+                    <div class="avatar">
+                        <img src="docs/assets/img/hery.jpg" alt="" />
+                    </div>
+                    <div class="info">
+                        <div class="title">
+                            <a href="http://www.marcellop.com">Bowling - une première à Mada</a>
+                        </div>
+                        <p>
+                            Soyez parmis les premières à essayer le bowling à Madagscar. Un endroit convivial, des rencontres aves de nouveaux personnes, autours d'un menu exceptionnel.
+                        </p>
+                    </div>
+                    <address>
+                        <strong>Twitter, Inc.</strong><br>
+                        795 Folsom Ave, Suite 600<br>
+                        San Francisco, CA 94107<br>
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                        <a href="mailto:#">first.last@example.com</a>
+                    </address>
+
+                </div>
+
+
+
+            </div>
+
+            <div class="span4">
+                <div id="card-features" class="card">
+                    <div class="card-media-container">
+                        <img src="http://fakeimg.pl/350x200/ff0000,128/000,255">
+                    </div>
+                    <div class="card-body">
+                        <h2>Star recrute un chef d'agence</h2>
+                        <p>A 12-column responsive <a href="./scaffolding.html#gridSystem">grid</a>, dozens of components, <a href="./javascript.html">JavaScript plugins</a>, typography, form controls, and even a <a href="./customize.html">web-based Customizer</a> to make Bootplus your own.</p>
+                    </div>
+                    <div class="card-actions">
+                        <a class="btn btn-success" href="getting-started.html#examples">
+                            Découvrir
+                            <i class="icon-circle-arrow-right"></i>
+                        </a>
+                        <a class="btn btn-warning" href="getting-started.html#examples">
+                            Proposer
+                        </a>
+                        <a class="btn btn-small pull-right" href="#"><i class="icon-star"></i></a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <h3 class="card-heading simple">Je met en valeur mes talents</h3>
+                    <div class="card-body simple">
+                        <p>Construisez votre image et mettez en évidence vos réalisation pour que les rectureurs et investisseurs le vois. Et n'hesitez pas à proposez vos projets.</p>
+                    </div>
+                    <div class="card-actions">
+                        <a class="btn btn-primary" href="getting-started.html#examples">
+                            Mon Profil
+                            <i class="icon-bookmark"></i>
+                        </a>
+                        <a class="btn btn-success" href="getting-started.html#examples">
+                            Voir des offres
+                            <i class="icon-list"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-media">
+                        <a class="card-media-container" href="#">
+                            <img src="http://fakeimg.pl/350x200/ff0000,128/000,255">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <h2>Built with Bootstrap.</h2>
+                        <p>Bootplus is built on top of <a href="http://twitter.github.io/bootstrap/">Twitter Bootstrap</a> as a customization that reproduce the most recent Google+ style.</p>
+                        <p>Bootplus utilizes <a href="http://lesscss.org">LESS CSS</a>, is compiled via <a href="http://nodejs.org">Node</a>, and is managed through <a href="http://github.com">GitHub</a> to help nerds do awesome stuff on the web.</p>
+                    </div>
+                    <div class="card-actions">
+                        <a class="btn btn-success" href="getting-started.html#examples">
+                            Découvrir
+                            <i class="icon-circle-arrow-right"></i>
+                        </a>
+                        <a class="btn btn-small pull-right" href="#"><i class="icon-star"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="span4">
+                <div id="card-features" class="card">
+                    <div class="card-media-container">
+                        <img src="http://fakeimg.pl/350x200/ff0000,128/000,255">
+                    </div>
+                    <div class="card-body">
+                        <h2>Packed with features.</h2>
+                        <p>A 12-column responsive <a href="./scaffolding.html#gridSystem">grid</a>, dozens of components, <a href="./javascript.html">JavaScript plugins</a>, typography, form controls, and even a <a href="./customize.html">web-based Customizer</a> to make Bootplus your own.</p>
+                    </div>
+                    <div class="card-actions">
+                        <a class="btn btn-success" href="getting-started.html#examples">
+                            Découvrir
+                            <i class="icon-circle-arrow-right"></i>
+                        </a>
+                        <a class="btn btn-small pull-right" href="#"><i class="icon-star"></i></a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-media">
+                        <a class="card-media-container" href="#">
+                            <img src="http://fakeimg.pl/350x200/ff0000,128/000,255">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <h2>Built with Bootstrap.</h2>
+                        <p>Bootplus is built on top of <a href="http://twitter.github.io/bootstrap/">Twitter Bootstrap</a> as a customization that reproduce the most recent Google+ style.</p>
+                        <p>Bootplus utilizes <a href="http://lesscss.org">LESS CSS</a>, is compiled via <a href="http://nodejs.org">Node</a>, and is managed through <a href="http://github.com">GitHub</a> to help nerds do awesome stuff on the web.</p>
+                    </div>
+                    <div class="card-actions">
+                        <a class="btn btn-success" href="getting-started.html#examples">
+                            Découvrir
+                            <i class="icon-circle-arrow-right"></i>
+                        </a>
+                        <a class="btn btn-small pull-right" href="#"><i class="icon-star"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<?php include('includes/footer.php') ?>
